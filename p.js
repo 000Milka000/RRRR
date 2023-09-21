73aabcd8b4fd974c23f359948781ac04e18322fe
000Milka000 <vicktoriaposysaeva@yandex.ru>
2023-09-21 18:40:20 +0300

    Фиксация 2.0

--- p.js
+++ p.js
@@ -26,16 +26,116 @@
 // let us3 = new User("Игорь");
 // us3.us();
 
-class User {
-    constructor(name, surname){
-        this.name = name,
-        this.surname = surname
-    }
 
-    us(){
-       confirm(`Привет ${this.name}. Твоя фамилия ${this.surname}?`)? alert (`Привет ${this.name} ${this.surname}.`) : alert ("Запишите пожалуйста по новой свои данные.")
+
+
+// class User {
+//     constructor(name, surname){
+//         this.name = name,
+//         this.surname = surname
+//     }
+
+//     us(){
+//        confirm(`Привет ${this.name}. Твоя фамилия ${this.surname}?`)? alert (`Привет ${this.name} ${this.surname}.`) : alert ("Запишите пожалуйста по новой свои данные.")
+//     }
+// }
+
+// let user = new User("Вика", "Посысаева");
+// user.us();
+
+
+
+
+// class User {
+//     #name
+
+//     constructor(name){
+//         this.#name = name
+//     }
+
+//     getName(){
+//         console.log(`Hello, ${this.#name}`)
+//     }
+// };
+
+// let us = new User("Vika");
+// console.log(us.name);
+
+// us.getName();
+
+
+
+// class Book {
+//     constructor(name, avtor, old){
+//         this.name = name,
+//         this.avtor = avtor,
+//         this.old = old
+//     }
+
+//     static com(ar1, ar2){
+//         if (ar1.old > ar2.old){
+//             console.log(`${ar1.name}, avtor: ${ar1.avtor}`)
+//         } else {
+//             console.log(`${ar2.name}, avtor: ${ar2.avtor} `)
+//         }
+//     }
+// }
+
+// let u1 = new Book("Metro 2033", "Gluhovskii", 12);
+// let u2 = new Book("Loko", "Ivanov", 5);
+
+// Book.com(u1, u2)
+
+
+
+
+
+// class User {
+//     constructor(name, surname){
+//         this.name = name,
+//         this.surname = surname
+//     }
+
+//     u1(){
+//         console.log(`${this.name}  ${this.surname}`)
+//     }
+
+// }
+
+// class User2 extends User{
+//     constructor(name, surname){
+//         super(name,surname)
+//     }
+
+//     u2(){
+//         console.log(`${this.name}__${this.surname}`)
+//     }
+// }
+
+
+// let us2 = new User2("Vika", "Posysaeva");
+// us2.u2();
+
+// let us1 = new User("Tatiana", "Petruhina");
+// us1.u1();
+
+
+class User {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    } 
+
+    static old (user1, user2){
+        if (user1.age > user2.age){
+            console.log(user1.name)
+        } else {
+            console.log(user2.name)
+        }
     }
 }
 
-let user = new User("Вика", "Посысаева");
-user.us();
+let user1 = new User("Vika", 20);
+let user2 = new User("Georgii", 22);
+
+User.old(user1, user2)
