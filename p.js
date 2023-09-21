a733f351853be06db8c7c0e8cf00f89b010ed9b8
000Milka000 <vicktoriaposysaeva@yandex.ru>
2023-09-21 13:53:41 +0300

    ФФФиксация

--- p.js
+++ p.js
@@ -1,27 +1,41 @@
+// class User {
+//     constructor(name, vn, dirs){
+//         this.name = name,
+//         this.vn = vn, 
+//         this.dirs = dirs
+//     }
+
+//     us(){
+//         if (dirs == "true"){
+//             console.log(`Здравствуйте директор ${this.name}. Вы ${this.vn} выглядите. `)
+//         } else if (dirs == "false"){
+//             console.log(`Здраствуй, гость ${this.name}. Ты ${this.vn} выглядишь.`)
+//         } else {
+//             console.log(`Пошёл нахуй отсюда ${this.name}`)
+//         }
+//     };
+
+// };
+
+// let us1 = new User("Виктория", "прекрасно", true);
+// us1.us();
+
+// let us2 = new User("Татьяна", "великолепно", false);
+// us2.us();
+
+// let us3 = new User("Игорь");
+// us3.us();
+
 class User {
-    constructor(name, vn, dirs){
+    constructor(name, surname){
         this.name = name,
-        this.vn = vn, 
-        this.dirs = dirs
+        this.surname = surname
     }
 
     us(){
-        if (dirs == "true"){
-            console.log(`Здравствуйте директор ${this.name}. Вы ${this.vn} выглядите. `)
-        } else if (dirs == "false"){
-            console.log(`Здраствуй, гость ${this.name}. Ты ${this.vn} выглядишь.`)
-        } else {
-            console.log(`Пошёл нахуй отсюда ${this.name}`)
-        }
-    };
+       confirm(`Привет ${this.name}. Твоя фамилия ${this.surname}?`)? alert (`Привет ${this.name} ${this.surname}.`) : alert ("Запишите пожалуйста по новой свои данные.")
+    }
+}
 
-};
-
-let us1 = new User("Виктория", "прекрасно", true);
-us1.us();
-
-let us2 = new User("Татьяна", "великолепно", false);
-us2.us();
-
-let us3 = new User("Игорь");
-us3.us();
+let user = new User("Вика", "Посысаева");
+user.us();
